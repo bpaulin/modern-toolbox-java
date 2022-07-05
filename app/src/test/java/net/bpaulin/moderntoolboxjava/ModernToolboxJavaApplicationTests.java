c407a67b3ed085386c969e1373ad4c5143a67d46
Bruno Paulin <brunopaulin@bpaulin.net>
2022-07-05 20:24:51 +0200

    app init

---
+++ app/src/test/java/net/bpaulin/moderntoolboxjava/ModernToolboxJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bpaulin.moderntoolboxjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernToolboxJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
